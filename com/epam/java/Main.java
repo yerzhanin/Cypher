f55d4255ac9ec8313110b314b80da6a9d449d69a
yerzhanin <yerzhan.int@gmail.com>
2018-12-03 20:48:02 +0600

    first

---
+++ com/epam/java/Main.java
@@ -0,0 +1,25 @@
+package com.epam.java;
+
+public class Main {
+
+    private static final char[] upper =
+            {'A','B','C','D','E','F','G','H','I','J','K','L','M','N', 'O','P','Q',
+                    'R','S','T','U','V','W','X','Y','Z'};
+
+    private static final char[] lower= {'a','b','c','d','e','f','g','h',
+            'i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    private static int n = 26;
+    private static int k = 3; // Shift value
+
+
+    public static void main(String[] args) {
+        String input = "Epam";
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < input.length(); ++i) {
+            char c = input.charAt(i);
+        }
+    }
+
+}
